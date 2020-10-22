8fd9027e428969c2e8a018c4204100ba0043dda7
Momozor <momozor4@gmail.com>
2020-10-22 15:42:21 +0800

    Broken Option

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct Specification {
     maintainer: String,
     target_version: String,
     status: String,
-    requirements: Vec<Requirement>,
+    requirements: Option<Vec<Requirement>>,
 }
 
 impl Requirement {
@@ -26,25 +26,6 @@ impl Requirement {
             name: name.to_string(),
             explanation: explanation.to_string(),
             priority: priority.to_string(),
-        }
-    }
-}
-
-impl Specification {
-    fn new(
-        name: &str,
-        maintainer: &str,
-        target_version: &str,
-        status: &str,
-        requirements: &mut Vec<Requirement>
-    ) -> Self {
-        Specification {
-            name: name.to_string(),
-            last_revised: chrono::Utc::today(),
-            maintainer: maintainer.to_string(),
-            target_version: target_version.to_string(),
-            status: status.to_string(),
-            requirements: requirements.to_vec(),
         }
     }
 }
@@ -63,14 +44,14 @@ fn main() -> Result<(), promptly::ReadlineError> {
     let target_version: String = promptly::prompt("Target Version")?;
     let status: String = promptly::prompt("Status")?;
 
-    let mut spec = Specification::new(
-        &name,
-        &maintainer,
-        &target_version,
-        &status,
-        &mut vec![Requirement::new("", "", "")],
-
-    );
+    let mut spec = Specification {
+        name: name,
+        last_revised: chrono::Utc::today(),
+        maintainer: maintainer,
+        target_version: target_version,
+        status: status,
+        requirements: None,
+    };
 
     let mut want_new_requirement = true;
     while want_new_requirement {
@@ -82,15 +63,10 @@ fn main() -> Result<(), promptly::ReadlineError> {
         else if do_proceed {
             &spec.requirements.push(match show_requirement_prompt() {
                 Ok(requirement) => requirement,
-                Err(_) => panic!("Requirement empty!"),
+                Err(_) => panic!("Requirement is empty!"),
             });
         }
     }
-
-    println!("{}", spec.last_revised);
-    for requirement in spec.requirements.iter() {
-        println!("{}", requirement.name);
-    }
-
+    
     Ok(())
 }
