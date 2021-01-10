88e460746c58db83181055e3851d1eaadc2250e3
Momozor <momozor4@gmail.com>
2021-01-11 01:23:57 +0800

    More progress for testing
    Signed-off-by: Momozor <momozor4@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,83 +12,80 @@ extern crate serde_yaml;
 #[macro_use] extern crate prettytable;
 extern crate promptly;
 
-mod moisrs {
-    const SPECIFICATION_FILENAME: &str = "SPECIFICATION";
+const SPECIFICATION_FILENAME: &str = "SPECIFICATION";
 
-    #[derive(Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-    struct Requirement {
-        name: String,
-        explanation: String,
-        priority: String,
+#[derive(Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct Requirement {
+    name: String,
+    explanation: String,
+    priority: String,
+}
+
+mod moisrs_date {
+    use chrono::{DateTime, Utc, TimeZone};
+    use serde::{self, Deserialize, Serializer, Deserializer};
+
+    const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S UTC";
+
+    pub fn serialize<S>(
+        date: &DateTime<Utc>,
+        serializer: S,
+    ) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let s = format!("{}", date.format(FORMAT));
+        serializer.serialize_str(&s)
     }
 
-    mod moisrs_date {
-        use chrono::{DateTime, Utc, TimeZone};
-        use serde::{self, Deserialize, Serializer, Deserializer};
-
-        const FORMAT: &'static str = "%Y-%m-%d %H:%M:%S UTC";
-
-        pub fn serialize<S>(
-            date: &DateTime<Utc>,
-            serializer: S,
-        ) -> Result<S::Ok, S::Error>
-        where
-            S: Serializer,
-        {
-            let s = format!("{}", date.format(FORMAT));
-            serializer.serialize_str(&s)
-        }
-
-        pub fn deserialize<'de, D>(
-            deserializer: D,
-        ) -> Result<DateTime<Utc>, D::Error>
-        where
-            D: Deserializer<'de>,
-        {
-            let s = String::deserialize(deserializer)?;
-            Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
-        }
+    pub fn deserialize<'de, D>(
+        deserializer: D,
+    ) -> Result<DateTime<Utc>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Utc.datetime_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
     }
+}
 
-    #[derive(Clone, PartialEq, serde::Serialize, serde::Deserialize)]
-    struct Specification {
-        name: String,
+#[derive(Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+pub struct Specification {
+    name: String,
 
-        #[serde(with = "moisrs_date")] 
-        last_revised: chrono::DateTime<chrono::Utc>,
-        maintainer: String,
-        target_version: String,
-        status: String,
-        requirements: Vec<Requirement>,
-    }
+    #[serde(with = "moisrs_date")] 
+    last_revised: chrono::DateTime<chrono::Utc>,
+    maintainer: String,
+    target_version: String,
+    status: String,
+    requirements: Vec<Requirement>,
+}
 
-    impl Requirement {
-        fn new(name: &str, explanation: &str, priority: &str) -> Self {
-            Requirement {
-                name: name.to_string(),
-                explanation: explanation.to_string(),
-                priority: priority.to_string(),
-            }
+impl Requirement {
+    pub fn new(name: &str, explanation: &str, priority: &str) -> Self {
+        Requirement {
+            name: name.to_string(),
+            explanation: explanation.to_string(),
+            priority: priority.to_string(),
         }
     }
+}
 
-    impl Specification {
-        fn new(
-            name: &str,
-            maintainer: &str,
-            target_version: &str,
-            status: &str,
-            requirements: &mut Vec<Requirement>
-        ) -> Self {
-            Specification {
-                name: name.to_string(),
-                last_revised: chrono::Utc::now(),
-                maintainer: maintainer.to_string(),
-                target_version: target_version.to_string(),
-                status: status.to_string(),
-                requirements: requirements.to_vec(),
-            }
+impl Specification {
+    pub fn new(
+        name: &str,
+        maintainer: &str,
+        target_version: &str,
+        status: &str,
+        requirements: &mut Vec<Requirement>
+    ) -> Self {
+        Specification {
+            name: name.to_string(),
+            last_revised: chrono::Utc::now(),
+            maintainer: maintainer.to_string(),
+            target_version: target_version.to_string(),
+            status: status.to_string(),
+            requirements: requirements.to_vec(),
         }
     }
-
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,1 +1,6 @@
 use moisrs;
+
+#[test]
+fn test_requirement_creation() {
+    assert_eq!(4, moisrs::Requirement::new("Fire", "tango", "soup"))
+}
